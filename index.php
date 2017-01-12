9d9a6a88b608cff89db64ad01477ce06b8da9cf5
roger.kikkas <roger.kikkas@gmail.com>
2017-01-12 13:24:22 +0200

    Fixes

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <?php
+// index.php
 /**
  * Created by PhpStorm.
  * User: roger.kikkas
  * Date: 12.01.2017
  * Time: 12:58
  */
-// create and template object
+// create an template object
 define('CLASSES_DIR', 'classes/');
 define('TMPL_DIR', 'tmpl/');
 require_once CLASSES_DIR.'template.php';
 // and use it
 // create an empty template object
-$tmpl = new template();
 // set up the file name for template
-$tmpl->file = 'main.html';
+// load template file content
+$tmpl = new template('main.html');
 // control the content of template object
-$tmpl->file = 'main.html';
 echo '<pre>';
 print_r($tmpl);
 echo '</pre>';
-
-
 ?>
