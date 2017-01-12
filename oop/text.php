ad2a545dcf58aac2ca80501223c94ec6945131fa
roger.kikkas <roger.kikkas@gmail.com>
2017-01-12 11:16:14 +0200

    Creade text class initial version

---
+++ oop/text.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: roger.kikkas
+ * Date: 12.01.2017
+ * Time: 10:51
+ */
+class text {// begin of class
+    // class variable
+    var $str = '';
+    // class methods
+    // set text method
+    function setText($s){
+        $this->str = $s;
+    }//setText
+
+    // show text output
+    function show(){
+        echo $this->str.'<br/>';
+    }// show
+}// end of class
+?>
