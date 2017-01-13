47aeb2ca6a74988769548bdc223af0130a90b5fc
roger.kikkas <roger.kikkas@gmail.com>
2017-01-13 12:07:52 +0200

    Create initial version of color text object

---
+++ oop/ctext.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: roger.kikkas
+ * Date: 13.01.2017
+ * Time: 12:04
+ */
+// import text class variables and methods
+require_once 'text.php';
+class ctext extends text
+{// begin of class
+
+}// end of class
+?>
