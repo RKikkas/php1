acf2ad1e91c3e630e152989352c6532af8754312
roger.kikkas <roger.kikkas@gmail.com>
2017-01-13 11:48:23 +0200

    Create initial version of main page style sheet

--- index.php
+++ index.php
@@ -6,9 +6,11 @@
  * Date: 12.01.2017
  * Time: 12:58
  */
+echo '<link rel="stylesheet" type="text/css" href="css/style.css">';
 // create an template object
 define('CLASSES_DIR', 'classes/');
 define('TMPL_DIR', 'tmpl/');
+define('STYLE_DIR', 'css/');
 require_once CLASSES_DIR.'template.php';
 // and use it
 // create an empty template object
@@ -16,14 +18,17 @@ require_once CLASSES_DIR.'template.php';
 // load template file content
 $tmpl = new template('main');
 // add pairs of template element names and real values
+$tmpl->set('style', STYLE_DIR.'main'.'.css');
+
+$tmpl->set('title', 'minu lehe pealkiri');
 $tmpl->set('menu', 'minu menüü');
 $tmpl->set('nav_bar', 'minu navigatsioon');
 $tmpl->set('lang_bar', 'minu keeleriba');
 $tmpl->set('content', 'minu sisu');
 // control the content of template object
-echo '<pre>';
+/* echo '<pre>';
 print_r($tmpl);
-echo '</pre>';
+echo '</pre>'; */
 
 echo $tmpl->parse();
 ?>
