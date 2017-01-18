dced1da53c019da30ad7c7b78dda9525ac129192
roger.kikkas <roger.kikkas@gmail.com>
2017-01-18 16:18:43 +0200

    Create data pairs for url

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -21,5 +21,11 @@ class linkobject extends http
         parent::__construct(); // import http class construct
         $this->baseUrl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
     }// construct
+    // create http data pairs and merge them
+    // pair is element_name=element_value, for example user=test
+    // name1=value1&name2=value2
+    function addToLink($link, $name, $val){
+        $link = $name.$this->eq.$val;
+    }// addToLink
 }// class end
 ?>
