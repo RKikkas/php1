e9a86dc376b60bb6d9d8e3e5e0c039ba34ff540f
roger.kikkas <roger.kikkas@gmail.com>
2017-01-19 10:44:01 +0200

    Allow to use subdirectories in template directory

--- classes/template.php
+++ classes/template.php
@@ -46,11 +46,15 @@ class template
         if(file_exists($f) and is_file($f) and is_readable($f)) {
             $this->readFile($f);
         }
+        // if we use subdirectories: /tmpl/dir/file.html - tmpl/dir.file
+        $f = TMPL_DIR.str_replace('.', '/', $this->file).'.html';
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->readFile($f);
+        } // subdirectories
         if($this->content === false){
             echo 'Ei saanud lugeda faili '.$this->file.'.<br/>';
             exit;
         }
-
     }// loadFile
     function readFile($f){
         $this->content = file_get_contents($f);
---
+++ menu.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roger.kikkas
+ * Date: 19.01.2017
+ * Time: 10:36
+ */
+// create menu and item objects
+$menu = new template(TMPL_DIR.'menu/menu');
+?>
