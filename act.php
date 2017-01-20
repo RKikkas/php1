f7195cfb66ff087eb05d761898f716f200942852
roger.kikkas <roger.kikkas@gmail.com>
2017-01-20 12:49:38 +0200

    Create default act initial version

--- act.php
+++ act.php
@@ -8,9 +8,11 @@
 $act = $http->get('act'); // act value from url
 // create act file path according to act value
 $fn = ACTS_DIR.str_replace('.', '/', $act). '.php';
-if(file_exists($fn) and is_file($fn) is_readable($fn)){
+if(file_exists($fn) and is_file($fn) and is_readable($fn)){
     require_once $fn;
 } else {
-
+    $fn = ACTS_DIR.DEFAULT_ACT.'.php';
+    $http->set('act', DEFAULT_ACT);
+    require_once $fn;
 }
 ?>
--- index.php
+++ index.php
@@ -25,7 +25,8 @@ require_once 'menu.php';
 // end of menu
 $tmpl->set('nav_bar', 'minu navigatsioon');
 $tmpl->set('lang_bar', 'minu keeleriba');
-$tmpl->set('content', 'minu sisu');
+// $tmpl->set('content', 'minu sisu');
+$tmpl->set('content', $http->get('content'));
 // output template content set up with real values
 echo $tmpl->parse();
 // database object test output
