ccf02197193248a351c41cb2ef9b49cb738302cb
roger.kikkas <roger.kikkas@gmail.com>
2017-02-13 09:06:23 +0200

    Create login template and login php

---
+++ acts/login.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roger.kikkas
+ * Date: 13.02.2017
+ * Time: 9:03
+ */
+$form = new Template('login');
+
+$form->set('error', $sess->get('login_error'));
+$sess->del('login_error');
+
+$form->set('submit', tr('Logi sisse'));
+$form->set('username_str', tr('Kasutajanimi'));
+$form->set('password_str', tr('Parool'));
+
+$form->set('username', $http->get('username', true));
+
+$link = $http->getLink(array('act' => 'login_do'));
+$form->set('action', $link);
+
+$tmpl->set('content', $form->parse());
+?>
---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roger.kikkas
+ * Date: 13.02.2017
+ * Time: 8:59
+ */
+header('Location: ../index.php?act=login');
+exit;
+?>
