6830d6d9be314e17caf458e848259ce7ea3fe6c3
roger.kikkas <roger.kikkas@gmail.com>
2017-02-13 10:13:01 +0200

    Create lang_en.php

---
+++ lang/lang_en.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roger.kikkas
+ * Date: 13.02.2017
+ * Time: 10:09
+ */
+// /lang/lang_en.php
+$_trans = array(
+    'Esimene' => 'First',
+    'Teine' => 'Second',
+    'Kolmas' => 'Third',
+    'Minu lehe pealkiri' => 'My page heading',
+    'Logi välja' => 'Logout',
+    'Kasutajanimi' => 'Username',
+    'Parool' => 'Password',
+    'Logi sisse' => 'Login',
+    'Viga sisselogimisel' => 'Login error'
+);
+?>
